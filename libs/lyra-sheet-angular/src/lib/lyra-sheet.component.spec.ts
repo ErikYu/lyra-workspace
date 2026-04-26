9a3c946f396f391e508145bf64fce24c57e7ade1
Erik Yu <erikyuyz@gmail.com>
2026-04-26 23:24:24 +0800

    chore: remove uesless code

--- libs/lyra-sheet-angular/src/lib/lyra-sheet.component.spec.ts
+++ libs/lyra-sheet-angular/src/lib/lyra-sheet.component.spec.ts
@@ -1,24 +1,103 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Data, DatasheetConfig } from '@lyra-sheet/core';
+import { LyraSheetVanilla } from '@lyra-sheet/vanilla';
 
-import { LyraSheetComponent } from './lyra-sheet.component';
+import { LyraSheetModule } from './lyra-sheet.module';
+
+const mockMount = jest.fn();
+const mockUpdate = jest.fn();
+const mockDestroy = jest.fn();
+
+jest.mock('@lyra-sheet/vanilla', () => ({
+  LyraSheetVanilla: jest.fn().mockImplementation(() => ({
+    mount: mockMount,
+    update: mockUpdate,
+    destroy: mockDestroy,
+  })),
+}));
+
+const data: Data = {
+  sheets: [
+    {
+      name: 'Sheet1',
+      selected: true,
+      data: {
+        merges: [],
+        rows: {},
+        rowCount: 10,
+        cols: {},
+        colCount: 5,
+      },
+    },
+  ],
+};
+
+const config: DatasheetConfig = {
+  width: () => 800,
+  height: () => 400,
+  row: {
+    height: 25,
+    count: 10,
+    indexHeight: 25,
+  },
+  col: {
+    width: 100,
+    count: 5,
+    indexWidth: 60,
+  },
+};
+
+@Component({
+  template: `<lyra-sheet [data]="data" [config]="config"></lyra-sheet>`,
+})
+class TestHostComponent {
+  data: Data = data;
+  config: DatasheetConfig = config;
+}
 
 describe('LyraSheetComponent', () => {
-  let component: LyraSheetComponent;
-  let fixture: ComponentFixture<LyraSheetComponent>;
+  let fixture: ComponentFixture<TestHostComponent>;
+  let host: TestHostComponent;
 
   beforeEach(async () => {
+    jest.clearAllMocks();
     await TestBed.configureTestingModule({
-      declarations: [LyraSheetComponent],
+      declarations: [TestHostComponent],
+      imports: [LyraSheetModule],
     }).compileComponents();
-  });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LyraSheetComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestHostComponent);
+    host = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('creates vanilla sheet and mounts on the host', () => {
+    expect(LyraSheetVanilla).toHaveBeenCalledWith({
+      data,
+      config,
+      onDataChange: expect.any(Function),
+    });
+    expect(mockMount).toHaveBeenCalledWith(expect.any(HTMLDivElement));
+  });
+
+  it('updates vanilla sheet when bound inputs change', () => {
+    const nextData: Data = {
+      sheets: [{ ...data.sheets[0], name: 'Renamed' }],
+    };
+    host.data = nextData;
+    fixture.detectChanges();
+
+    expect(mockUpdate).toHaveBeenLastCalledWith({
+      data: nextData,
+      config,
+      onDataChange: expect.any(Function),
+    });
+  });
+
+  it('destroys vanilla sheet on destroy', () => {
+    fixture.destroy();
+
+    expect(mockDestroy).toHaveBeenCalledTimes(1);
   });
 });
--- libs/lyra-sheet-angular/src/lib/lyra-sheet.component.ts
+++ libs/lyra-sheet-angular/src/lib/lyra-sheet.component.ts
@@ -1,79 +1,100 @@
 import {
+  AfterViewInit,
   Component,
   ElementRef,
   EventEmitter,
   Input,
-  OnInit,
+  NgZone,
+  OnChanges,
+  OnDestroy,
   Output,
+  SimpleChanges,
+  ViewChild,
 } from '@angular/core';
 import { Data, DatasheetConfig } from '@lyra-sheet/core';
-import { fromEvent } from 'rxjs';
-import { startWith } from 'rxjs/operators';
-import { cloneDeep } from '@lyra-sheet/core';
-import { BaseContainer } from './services/_base_container';
+import { LyraSheetVanilla } from '@lyra-sheet/vanilla';
+
+const defaultConfig: DatasheetConfig = {
+  width: () => document.documentElement.clientWidth,
+  height: () => document.documentElement.clientHeight,
+  row: {
+    height: 25,
+    count: 100,
+    indexHeight: 25,
+  },
+  col: {
+    width: 100,
+    count: 30,
+    indexWidth: 60,
+  },
+};
+
+const defaultData: Data = {
+  sheets: [
+    {
+      name: 'Sheet1',
+      data: {
+        merges: [],
+        rows: {},
+        rowCount: 100,
+        cols: {},
+        colCount: 30,
+      },
+      selected: true,
+    },
+  ],
+};
 
 @Component({
   selector: 'lyra-sheet',
   templateUrl: './lyra-sheet.component.html',
   styleUrls: ['./lyra-sheet.component.scss'],
-  providers: [BaseContainer],
-  host: { class: 'lyra-sheet' },
 })
-export class LyraSheetComponent implements OnInit {
-  @Input() config: DatasheetConfig = {
-    width: () => document.documentElement.clientWidth,
-    height: () => document.documentElement.clientHeight,
-    row: {
-      height: 25,
-      count: 100,
-      indexHeight: 25,
-    },
-    col: {
-      width: 100,
-      count: 30,
-      indexWidth: 60,
-    },
-  };
+export class LyraSheetComponent
+  implements AfterViewInit, OnChanges, OnDestroy
+{
+  @ViewChild('host', { static: true })
+  hostRef!: ElementRef<HTMLDivElement>;
 
-  @Input() data: Data = {
-    sheets: [
-      {
-        name: 'Sheet1',
-        data: {
-          merges: [],
-          rows: {},
-          rowCount: 100,
-          cols: {},
-          colCount: 30,
-        },
-        selected: true,
-      },
-    ],
-  };
+  @Input() config: DatasheetConfig = defaultConfig;
+  @Input() data: Data = defaultData;
 
   @Output() dataChange = new EventEmitter<Data>();
-  constructor(
-    private container: BaseContainer,
-    private el: ElementRef<HTMLElement>,
-  ) {}
 
-  ngOnInit(): void {
-    document.execCommand('styleWithCSS', false, true as never);
-    this.container.elementRefService.init(this.el.nativeElement);
-    this.container.configService.setConfig(this.config);
-    fromEvent(window, 'resize')
-      .pipe(startWith({}))
-      .subscribe(() => {
-        this.el.nativeElement.style.width = `${this.config.width()}px`;
-        this.el.nativeElement.style.height = `${this.config.height()}px`;
-        this.container.configService.resize(this.el.nativeElement);
-      });
-    const initialData = cloneDeep(this.data);
-    this.container.dataService.loadData(initialData);
-    this.container.historyService.init(initialData);
-    this.container.viewRangeService.init();
-    this.container.dataService.dataChanged$.subscribe((res: Data) => {
-      this.dataChange.emit(res);
+  private sheet: LyraSheetVanilla | null = null;
+  private viewReady = false;
+
+  constructor(private ngZone: NgZone) {}
+
+  ngAfterViewInit(): void {
+    document.execCommand?.('styleWithCSS', false, true as never);
+    this.sheet = new LyraSheetVanilla({
+      data: this.data,
+      config: this.config,
+      onDataChange: (next) =>
+        this.ngZone.run(() => this.dataChange.emit(next)),
     });
+    this.sheet.mount(this.hostRef.nativeElement);
+    this.viewReady = true;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!this.viewReady || !this.sheet) {
+      return;
+    }
+    if (changes['data'] || changes['config']) {
+      this.sheet.update({
+        data: this.data,
+        config: this.config,
+        onDataChange: (next) =>
+          this.ngZone.run(() => this.dataChange.emit(next)),
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.sheet?.destroy();
+    this.sheet = null;
+    this.viewReady = false;
   }
 }
