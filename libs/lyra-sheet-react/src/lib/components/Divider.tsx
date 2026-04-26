8750d37790641e12b90dd6845672d26b042f70c5
Erik Yu <erikyuyz@gmail.com>
2026-04-26 23:15:25 +0800

    chore: remove useless code

--- libs/lyra-sheet-react/src/lib/components/Divider.tsx
+++
@@ -1,7 +0,0 @@
-interface DividerProp {
-  direction: 'vertical' | 'horizontal';
-}
-
-export function LyraSheetDivider(prop: DividerProp) {
-  return <div className={'lyra-sheet-divider ' + prop.direction}></div>;
-}
